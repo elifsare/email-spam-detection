fdf10745fcd43f0ad3d3f8e56309094abedfd160
elifsare <2190656018@nku.edu.tr>
2022-09-06 01:58:34 +0300

    Update SpamMailPred.py

--- SpamMailPred.py
+++ SpamMailPred.py
@@ -14,8 +14,7 @@ print("satır, sütun --> " + str(mailData.shape))
 mailData.loc[mailData['Category'] == 'spam', 'Category',] = 0 # Category sütunundaki metin 'spam' ise bu değeri 0 ile değiştiriyorum
 mailData.loc[mailData['Category'] == 'ham', 'Category',] = 1 # Category sütunundaki metin 'ham' ise bu değeri 1 ile değiştiriyorum
 
-# verileri metin ve etiket olarak ayıracağız (separating the data as texts and label)
-# x tüm mesajlar y tüm etiketler
+# separating the data as texts and label
 X = mailData['Message']
 y = mailData['Category']
 print(X)
@@ -23,20 +22,14 @@ print(y)
 
 # split the data into training and testing sets
 from sklearn.model_selection import train_test_split
-# X deki verileri 2 ye böleceğiz ki train ve test için 2 ayrı şey olsun
-# y deki verileri 2 ye böleceğiz ki train ve test için 2 ayrı şey olsun
-# toplam 4 bölüm aşağıda;
 X_train, X_test, y_train, y_test = train_test_split(X,
                                                     y,
                                                     test_size = 0.2, #test için verinin %20'si kullanılacak train için %80
                                                     random_state = 3)  
 
-# Feature Extraction (sabit değerleri sayısal değere dönüştürdük)
+# Feature Extraction
 # transform the text data to feature vectors that can be used as input to the ligistic regression
-# metni sayısal değere dönüştürüyoruz
-# bu sayısal değerler modeli besleyecek yani giriş verisi olarak kullanılacak
-from sklearn.feature_extraction.text import TfidfVectorizer #vektörleştirici
+from sklearn.feature_extraction.text import TfidfVectorizer 
 feature_extraction = TfidfVectorizer(min_df = 1,
                                      stop_words = 'english',
                                      lowercase = 'True')
-# Fit the model
